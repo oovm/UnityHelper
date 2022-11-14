ba88ccd45f4a236342eece2a286972e5d66e4275
Aster <galaster@foxmail.com>
2022-11-14 14:30:48 +0800

    Rename to Zx Framework

---
+++ Runtime/Core/MonoSingleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static readonly Lazy<T> lazy = new(LazyCreate());
+        public static T Instance => lazy.Value;
+        protected abstract void Initialize();
+
+        private static T LazyCreate()
+        {
+            var owner = new GameObject($"Create Singleton {typeof(T).Name}");
+            var instance = owner.AddComponent<T>();
+            DontDestroyOnLoad(owner);
+            return instance;
+        }
+    }
+}
---
+++ Runtime/Core/Singleton.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Core
+{
+    public abstract class Singleton<T> where T : class, new()
+    {
+        private static readonly Lazy<T> lazy = new(() => new T());
+        public static T Instance => lazy.Value;
+        protected abstract void Initialize();
+    }
+}
