2a4abd0c3c010af02a919281c96c71b3ab999926
aster <aster@vers.site>
2023-01-11 11:35:01 +0800

    Add virtual device to map buttons as keyboard keys

---
+++ Runtime/Core/MockDevice.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+namespace Zx.Core
+{
+// Take a set of actions and create an InputDevice for it that has a control
+// for each of the actions. Also binds the actions to that those controls.
+    public static class MockDevice
+    {
+        public static void Keyboard(Key key, bool value)
+        {
+            var mockInput = UnityEngine.InputSystem.Keyboard.current[key];
+            using (StateEvent.From(mockInput.device, out var eventPtr))
+            {
+                eventPtr.time = InputState.currentTime;
+                mockInput.WriteValueIntoEvent(value ? 1f : 0f, eventPtr);
+
+                InputSystem.QueueEvent(eventPtr);
+            }
+        }
+    }
+}
---
+++ Runtime/UI/KeyboardRaycast.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Zx.Core;
+
+namespace Zx.UI
+{
+    /// <summary>
+    /// 点击该区域等价于按下键盘上的某个键
+    /// </summary>
+    [RequireComponent(typeof(CanvasRenderer))]
+    public class KeyboardRaycast : Graphic, IPointerDownHandler, IPointerUpHandler
+    {
+        public Key bindKey;
+
+        protected KeyboardRaycast()
+        {
+            useLegacyMeshGeneration = false;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            MockDevice.Keyboard(bindKey, true);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            MockDevice.Keyboard(bindKey, false);
+        }
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+        }
+    }
+}
